9ae8f9a318e557e754e5d6ae19fd936e0ee1f9e5
gamedev-lesnik <lesnik.gamedev@gmail.com>
2023-10-02 22:50:11 +0200

    add divions function

--- basic_operations/Arithmetics.cpp
+++ basic_operations/Arithmetics.cpp
@@ -3,15 +3,18 @@
 int add(int a, int b);
 int subtract(int a, int b);
 int multiply(int a, int b);
-
+float division(float a, float b);
 int main()
 {
 
     int a = 4, b = 2;
+    float a1 = 6, b1 = 2;
     std::cout << "number a = 4, number b = 2" << std::endl;
+    std::cout << "number a1 = 6, number b1 = 2" << std::endl;
     std::cout << "adding numbers a + b = " << add(a, b) << std::endl;
-    std::cout << "subtracting numbers b from a = " << subtract(a, b) << std::endl;
+    std::cout << "subtracting number b from a = " << subtract(a, b) << std::endl;
     std::cout << "multiply numbers a * b = " << multiply(a, b) << std::endl;
+    std::cout << "dividing number a1 by b1 = " << division(a1, b1) << std::endl;
     return 0;
 }
 // Addition (+): Adds two numbers.
@@ -28,4 +31,18 @@ int subtract(int a, int b)
 int multiply(int a, int b)
 {
     return a * b;
+}
+// Division (/); Divides one number from another.
+float division(float a, float b)
+{
+    if (b == 0)
+    {
+        std::cout << "Error: Divison by zero" << std::endl;
+        return 0;
+    }
+
+    else
+    {
+        return a / b;
+    }
 }
