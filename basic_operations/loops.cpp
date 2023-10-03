a377947847ab6adf0e34461eed023bf2a2e24c70
gamedev-lesnik <lesnik.gamedev@gmail.com>
2023-10-04 00:52:18 +0200

    add loops for, while, do while, range-based for

--- basic_operations/loops.cpp
+++ basic_operations/loops.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <set>
+#include <vector>
 
 int main()
 {
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << "." << std::endl;
+    }
+
+    while (true)
+    {
+        if (a == 10)
+        {
+            break;
+        }
+        std::cout << a << "." << std::endl;
+        a++;
+    }
+
+    do
+    {
+        std::cout << b << "." << std::endl;
+        b++;
+    } while (b <= 2);
+
+    // range-based for
+    int array[] = {1, 2, 3, 4, 5};
+    for (int num : array)
+    {
+        std::cout << num << " ";
+    }
+
+    std::set<std::string> array_char = {"one", "two", "three"};
+    for (const std::string &string_ : array_char)
+    {
+        std::cout << string_ << " ";
+    }
+
+    std::vector<std::string> array_char_ = {"one", "two", "three"};
+    for (const std::string &string_ : array_char_)
+    {
+        std::cout << string_ << " ";
+    }
     return 0;
+
 }
